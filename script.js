8d920a2990f50c894755e48aa0e9ea04ef09f8f9
gervinho520 <gervaisbimenyimana912@gmail.com>
2026-08-26 11:02:24 +0100

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const studentList = document.getElementById("studentList");
 const totalStudents = document.getElementById("totalStudents");
 const deleteAllBtn = document.getElementById("deleteAllBtn");
 
-const GOOGLE_SCRIPT_URL = "COLLE_ICI_TON_URL_APPS_SCRIPT";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyUTAJpBsCPpj62l86bmgBz97ApBjaopi8x_ZkRxhjQM5I9UR3nuIkcQeE9ysKHfL2y6Q/exec";
 
 let students = JSON.parse(localStorage.getItem("students")) || [];
 
